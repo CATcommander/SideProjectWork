bb8d019fe2387436178a9418f12075f610060878
harman <harman@localhost.localdomain>
2015-03-01 19:27:13 -0800

    added funtion prototypes & definitions

---
+++ twoDimArray.cpp
@@ -0,0 +1,26 @@
+// Harmanpreet S.
+// An example of dynamically allocated two-dimensional array 
+// FOR EDUCATIONAL PURPOSES ONLY
+
+#include <iostream>
+#include <iomanip>
+
+// fill array with numbers from user
+void fill_board(int ** b, int row_size, int col_size);
+void print_board(int ** b, int row_size, int col_size);
+
+int main()
+{
+        
+    return 0;
+}
+
+void fill_board(int ** b, int row_size, int col_size)
+{
+
+}
+
+void print_board(int ** b, int row_size, int col_size)
+{
+
+}
